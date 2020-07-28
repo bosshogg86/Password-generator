0c98f6cd66cbc30c389b3544638e70596b92e0ab
bosshogg86 <jeffhogg86@gmail.com>
2020-07-28 16:41:00 -0700

    reset characters and password to empty strings

--- Develop/script.js
+++ Develop/script.js
@@ -5,58 +5,64 @@ var generateBtn = document.querySelector("#generate");
 const lowercase = ["abcdefghijklmnopqrstuvwxyz"];
 const uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 const numeric = [0123456789];
-const special = ["~`!#$%^&*+=-[]\\\',;/{}|\":<>?"];
+const special = ["~`!#$%^&*+=-[]\\',;/{}|\":<>?"];
 let characters = "";
 let password = "";
 
 // Write password to the #password input
 function writePassword() {
-  let password = generatePassword(); 
+  let password = generatePassword();
   let passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
-
 
 // Added event listeners to generate button
 generateBtn.addEventListener("click", promptMe);
 generateBtn.addEventListener("click", writePassword);
 
-// Prompts/confirms
+// Prompts/confirms for character types
 function promptMe() {
-  passwordLength = parseInt(prompt("Choose your password length, between 8 and 128 characters")) 
-    while (passwordLength < 8 || passwordLength > 128) {
-      alert("Choose a number between 8 and 128")
-      passwordLength = parseInt(prompt("Choose your password length, between 8 and 128 characters"))
-    }
+  passwordLength = parseInt(
+    prompt("Choose your password length, between 8 and 128 characters")
+  );
+  while (passwordLength < 8 || passwordLength > 128) {
+    alert("Choose a number between 8 and 128");
+    passwordLength = parseInt(
+      prompt("Choose your password length, between 8 and 128 characters")
+    );
+  }
   confirmLower = confirm("Do you want lowercase letters?");
   confirmUpper = confirm("Do you want UPPERCASE letters?");
-  confirmNumeric = confirm("Do you want numbers?"); 
+  confirmNumeric = confirm("Do you want numbers?");
   confirmSpecial = confirm("Do you want special characters?");
-    while (!confirmLower && !confirmUpper && !confirmNumeric && !confirmSpecial) {
-      alert("You must select at least one character type!");
-      confirmLower = confirm("Do you want lowercase letters?");
-      confirmUpper = confirm("Do you want UPPERCASE letters?");
-      confirmNumeric = confirm("Do you want numbers?"); 
-      confirmSpecial = confirm("Do you want special characters?");
-    }  
+  while (!confirmLower && !confirmUpper && !confirmNumeric && !confirmSpecial) {
+    alert("You must select at least one character type!");
+    confirmLower = confirm("Do you want lowercase letters?");
+    confirmUpper = confirm("Do you want UPPERCASE letters?");
+    confirmNumeric = confirm("Do you want numbers?");
+    confirmSpecial = confirm("Do you want special characters?");
+  }
 }
 
 // Function to create allowed characters
 function generatePassword() {
+  let characters = "";
   if (confirmLower) {
     characters += lowercase;
-  } if (confirmUpper) {
+  }
+  if (confirmUpper) {
     characters += uppercase;
-  } if (confirmNumeric) {
+  }
+  if (confirmNumeric) {
     characters += numeric;
-  } if (confirmSpecial) {
+  }
+  if (confirmSpecial) {
     characters += special;
-  } 
-
-// Generates random string
-  for (let i = 0;  i < passwordLength; i++) {
-    password +=  characters[Math.floor(Math.random() * characters.length)];
+  }
+  let password = "";
+  // Generates random string
+  for (let i = 0; i < passwordLength; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
   }
   return password;
-} 
-
+}
